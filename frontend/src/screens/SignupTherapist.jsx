cea5d5638bda7e246d1211f3f398de3d93176e53
Anish949 <74363280+Anish949@users.noreply.github.com>
2023-09-30 03:38:16 +0545

    gender option

--- frontend/src/screens/SignupTherapist.jsx
+++ frontend/src/screens/SignupTherapist.jsx
@@ -363,7 +363,9 @@ export default function SignupTherapist() {
                         console.log(e.target.value);
                       }}
                     >
+                      <option value="">Choose Gender</option>
                       <option value="male">Male</option>
+
                       <option value="female">Female</option>
                       <option value="others">Other</option>
                     </select>
