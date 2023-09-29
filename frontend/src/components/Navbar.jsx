2b0e3a451e352d6cc9f6b85241e807991b047cec
sagar-joshi83 <joshhisagar9375@gmail.com>
2023-09-29 20:06:22 +0545

    806

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
                 type="button"
                 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
-                Signup
+                <Signup />
               </button>
             </Link>
             <button
