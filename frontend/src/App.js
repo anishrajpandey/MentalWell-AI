2da546864a95a7a74f8137ea4bd53382ff9f0ba2
Anish949 <74363280+Anish949@users.noreply.github.com>
2023-09-29 15:24:19 +0545

    signup form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Signup from "./screens/Signup";
+import Home from "./screens/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div className="App">mental well ai</div>;
+  return (
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route exact path="signup" element={<Signup />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
