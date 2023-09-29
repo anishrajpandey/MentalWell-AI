e6a0165a899372814f3a9db421dc4d04d2457680
Anish949 <74363280+Anish949@users.noreply.github.com>
2023-09-29 17:22:08 +0545

    color

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -59,7 +59,7 @@ const Navbar = () => {
             class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
             id="navbar-cta"
           >
-            <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-white  ">
+            <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-white  ">
               <Link
                 class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
                 aria-current="page"
