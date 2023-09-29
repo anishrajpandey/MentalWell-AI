74108e52dd29545409029c78f67624b3b638c839
sagar-joshi83 <joshhisagar9375@gmail.com>
2023-09-29 17:00:29 +0545

    5pm

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -59,7 +59,7 @@ const Navbar = () => {
             class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
             id="navbar-cta"
           >
-            <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-white  ">
               <Link
                 class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
                 aria-current="page"
@@ -71,8 +71,14 @@ const Navbar = () => {
                 <a
                   href="#"
                   class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500-gray-700-white md:dark:hover:bg-transparent-700"
+                ></a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500-gray-700-white md:dark:hover:bg-transparent-700"
                 >
-                  Community
+                  Forum
                 </a>
               </li>
               <li>
@@ -80,15 +86,7 @@ const Navbar = () => {
                   href="#"
                   class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500-gray-700-white md:dark:hover:bg-transparent-700"
                 >
-                  Services
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500-gray-700-white md:dark:hover:bg-transparent-700"
-                >
-                  Contact
+                  Find Therapist
                 </a>
               </li>
             </ul>
